8e3fddaae0eac75f5478433c8bc22e0dfc35bec0
jaskon139 <jaskon139@gmail.com>
2015-12-14 11:50:32 +0800

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -59,25 +59,6 @@ acceptor.on('request', function(req, res) {
           req.pipe(connector);
           req.resume();
   }
-  
-  
-  
-  
-    console.log('request ' + request.url);
-    request.pause();
-    var options = url.parse(request.url);
-    options.headers = request.headers;
-    options.method = request.method;
-    options.agent = false;
-
-    var connector = http.request(options, function(serverResponse) {
-            serverResponse.pause();
-            response.writeHeader(serverResponse.statusCode, serverResponse.headers);
-            serverResponse.pipe(response);
-            serverResponse.resume();
-    });
-    request.pipe(connector);
-    request.resume();
 });
 
 
