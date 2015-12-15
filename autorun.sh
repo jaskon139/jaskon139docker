a97817cae551be6c4a0c48b3c9802a0d9903b07e
jaskon139 <jaskon139@gmail.com>
2015-12-15 09:50:40 +0800

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #/usr/local/bin/hts --forward-port 127.0.0.1:22 80
 /etc/init.d/ssh start
-/etc/init.d/tinyproxy start
+#/etc/init.d/tinyproxy start
+/usr/bin/hts -F 127.0.0.1:22 4500
 cd /app
 node app.js -p 3000 &
 node proxy.js > text.log 2>1
